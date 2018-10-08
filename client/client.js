4429b378f046df73bf2bb715f0fde19c1ed9c383
Dawid Klatkiewicz <klatkiewiczdawid@holinhed.com>
2018-10-08 15:22:33 -0400

    ahh

--- client/client.js
+++ client/client.js
@@ -1,18 +1,46 @@
 const form = document.querySelector('form');
 const URL = 'http://localhost:5000/api';
 
+listYeets();
+
 form.addEventListener('submit', (event)=>{
     event.preventDefault();
-    getYeets();
+    
+    const formData = new FormData(form);
+    const name = formData.get('name');
+    const content = formData.get('content');
+    
+    const obj = {
+        name,
+        content,
+    }
+    
+    fetch(URL, {
+        method: 'POST',
+        body: JSON.stringify(obj),
+        headers: {
+          'content-type': 'application/json'
+        }
+      }).then(response => {      
+        if (!response.ok) {
+          const contentType = response.headers.get('content-type');
+          if (contentType.includes('json')) {
+            return response.json().then(error => Promise.reject(error.message));
+          } else {
+            return response.text().then(message => Promise.reject(message));
+          }
+        }
+      });
+    
+    listYeets();
 })
 
-function getYeets(){
-
+function listYeets(){
     fetch(URL)
-    .then(function(response) {
-      return response.json();
+    .then(function(res) {
+      return res.json();
     })
-    .then(function(myJson) {
-      console.table(myJson);
-    });
+    .then(function(content) {
+      console.table(content);
+    })
 }
--- server/index.js
+++ server/index.js
@@ -2,24 +2,38 @@ const PORT = 5000;
 
 const express = require('express');
 const cors = require('cors');
+const rateLimit = require('express-rate-limit');
 const app = express();
 
 app.use(cors());
-
-app.listen(PORT, ()=>{
-    console.log(`Server started on port ${PORT}`);
-});
-
-//response to / get request
-app.get('/', (req, res)=>{
-    res.send(JSON.stringify({
-        error: 501,
-        message: 'Whoopsie Doopsie! To use the API, please use /api'
-    }));
-});
 
 app.get('/api', (req, res)=>{
     res.send(JSON.stringify({
         message: "Request handled!"
     }));
+});
+
+app.use(rateLimit({
+    windowMs: 30 * 1000,
+    max: 1
+  }));
+
+app.post('/api', (req, res, next)=>{
+    res.json({
+        message: "post handle"
+    });
+});
+
+app.use((error, req, res, next) => {
+    res.status(500);
+    res.json({
+      message: error.message
+    });
+    res.send(JSON.stringify({
+        message: error.message
+    }))
+  });
+
+  app.listen(PORT, ()=>{
+    console.log(`Server started on port ${PORT}`);
 });
