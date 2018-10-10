0cb860b20b971852afe9c3c8616577e5ae929082
Dawid Klatkiewicz <klatkiewiczdawid@holinhed.com>
2018-10-10 19:53:18 -0400

    did stuff

--- client/client.js
+++ client/client.js
@@ -14,25 +14,33 @@ form.addEventListener('submit', (event)=>{
         name,
         content,
     }
-    
-    fetch(URL, {
-        method: 'POST',
-        body: JSON.stringify(obj),
-        headers: {
-          'content-type': 'application/json'
-        }
-      }).then(response => {      
-        if (!response.ok) {
-          const contentType = response.headers.get('content-type');
-          if (contentType.includes('json')) {
-            return response.json().then(error => Promise.reject(error.message));
-          } else {
-            return response.text().then(message => Promise.reject(message));
+    if (name != '' && content != '' && name.length <= 50 && content.length <= 250){
+      form.style.display = 'none';
+      fetch(URL, {
+          method: 'POST',
+          body: JSON.stringify(obj),
+          headers: {
+            'content-type': 'application/json'
           }
-        }
-      });
-    
-    listYeets();
+        }).then(response => {
+          if (!response.ok) {
+            const contentType = response.headers.get('content-type');
+            if (contentType.includes('json')) {
+              return response.json().then(error => Promise.reject(error.message));
+            } else {
+              return response.text().then(message => Promise.reject(message));
+            }
+          }
+        }).then(()=>{
+          form.reset();
+          setTimeout(() => {
+            form.style.display = '';
+          }, 30000);
+          listYeets();
+        });
+    }else{
+      alert("You need to actually enter stuff...");
+    }
 })
 
 function listYeets(){
--- server/index.js
+++ server/index.js
@@ -6,22 +6,43 @@ const rateLimit = require('express-rate-limit');
 const app = express();
 
 app.use(cors());
+app.use(express.json());
+
+app.get('/', (req, res) => {
+    res.json({
+      message: 'Whoops! Wrong page :('
+    });
+});
+
+//TODO: make this an actually database
+let yeets = [];
 
 app.get('/api', (req, res)=>{
     res.send(JSON.stringify({
-        message: "Request handled!"
+        yeets
     }));
 });
 
 app.use(rateLimit({
-    windowMs: 30 * 1000,
+    windowMs: 5 * 1000,
     max: 1
-  }));
+}));
 
 app.post('/api', (req, res, next)=>{
-    res.json({
-        message: "post handle"
-    });
+
+    const yeet = {
+        name: req.body.name.toString().trim(),
+        content: req.body.content.toString().trim(),
+        created: new Date()
+      };
+
+      if (yeet.name.length < 50 && yeet.content.length < 250){
+        yeets.push(yeet);
+      }
+
+      res.json({
+        message: yeet,
+      });
 });
 
 app.use((error, req, res, next) => {
@@ -29,11 +50,8 @@ app.use((error, req, res, next) => {
     res.json({
       message: error.message
     });
-    res.send(JSON.stringify({
-        message: error.message
-    }))
   });
 
-  app.listen(PORT, ()=>{
+app.listen(PORT, ()=>{
     console.log(`Server started on port ${PORT}`);
 });
